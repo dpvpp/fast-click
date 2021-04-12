ebc54f96213d24b9dfef28b116de011ae128731b
dpvpp <dpvpp22@yahoo.com>
2021-04-12 18:27:34 -0400

    Removed timer compnent

--- src/ClickCounter.js
+++ src/ClickCounter.js
@@ -1,12 +1,27 @@
-import React, {useState} from 'react';
-import Timer from './Timer'
+import React, {useState, useEffect} from 'react';
 
 function ClickCounter() {
     const[count, setCount] = useState(0);
     const[started, setStarted] = useState(false);
+    const[time, setTime] = useState(10);
+
+    useEffect(() => {
+        if(time > 0 && started) {  
+            setTimeout(decrTime, 1000);
+        } 
+    })
+
+    const decrTime = () => {
+        setTime(time - 1);
+        if(time === 0) {
+            setStarted(false);
+        }
+    } 
 
     const start = () => {
-        setStarted(true);
+        if(time === 10) { 
+            setStarted(true);
+        }
     }
 
     const incrCount = () => {
@@ -16,7 +31,7 @@ function ClickCounter() {
     return (
         <div class = "p-5 text-center bg-white">
             <h1 class="mb-3">How Fast Can You Click?</h1>
-            <Timer started={started} />
+            <h3>Time: {time}</h3>
             <h3>Click count: {count}</h3>
             <button class="btn btn-primary" onClick={started ? incrCount : start}>Click Me!</button>
         </div>
