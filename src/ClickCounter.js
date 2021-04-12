1732ce5eba071640250b31412852fdb65e3b60e5
dpvpp <dpvpp22@yahoo.com>
2021-04-12 13:14:25 -0400

    Timer added

--- src/ClickCounter.js
+++ src/ClickCounter.js
@@ -1,13 +1,11 @@
-import React, {useEffect, useState} from 'react';
+import React, {useState} from 'react';
+import Timer from './Timer'
 
 function ClickCounter() {
     const[count, setCount] = useState(0);
-    const[time, setTime] = useState(10);
     // const[started, setStarted] = useState(false);
 
-    // const decrTime = () => {
-    //     setTime(time - 1);
-    // }
+
 
     const incrCount = () => {
         setCount(count + 1);
@@ -16,7 +14,7 @@ function ClickCounter() {
     return (
         <div class = "p-5 text-center bg-white">
             <h1 class="mb-3">How Fast Can You Click?</h1>
-            <h3>Time: {time}</h3>
+            <Timer />
             <h3>Click count: {count}</h3>
             <button class="btn btn-primary" onClick={incrCount}>Click Me!</button>
         </div>
