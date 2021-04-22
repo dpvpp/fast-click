3a1076121b987b255f55245772c36c0cd16ee968
dpvpp <dpvpp22@yahoo.com>
2021-04-22 11:53:02 -0400

    Added some color to the timer

--- src/ClickCounter.js
+++ src/ClickCounter.js
@@ -41,10 +41,21 @@ function ClickCounter() {
     return (
         <div class = "p-5 text-center bg-white">
             <h1 class="mb-3">How Fast Can You Click?</h1>
-            <h3>Time: {time}</h3>
+            
+            <h3>
+                
+                {/* <button class="btn btn-danger" onClick={reset} style={{margin:'5px', fontSize:'25px'}}>--</button>
+                <button class="btn btn-danger" onClick={reset} style={{margin:'5px', fontSize:'25px'}}>-</button> */}
+                Time: 
+                <span style={{color: (started ? 'green' : 'black')}}>{time}</span>
+                {/* <button class="btn btn-success" onClick={reset} style={{margin:'5px', fontSize:'25px'}}>+</button>
+                <button class="btn btn-success" onClick={reset} style={{margin:'5px', fontSize:'25px'}}>++</button> */}
+            </h3>
+            
+
             <h3>Click count: {count}</h3>
             <button class="btn btn-danger" onClick={reset}>Reset</button>
-            <button class="btn btn-primary" onClick={started ? incrCount : start}>Click Me!</button>
+            <button class="btn btn-primary" onClick={started ? incrCount : start} >Click Me!</button>
         </div>
     )
 }
